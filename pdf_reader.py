8581bfc6b882bda0d089641b57979ba9224bad03
shibampokerail <95929007+shibampokerail@users.noreply.github.com>
2023-11-14 15:57:01 -0600

    Updated pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,11 +1,12 @@
 import pdfplumber
 
 def get_text_from_pdf(path):
-
+    content = ""
     with pdfplumber.open(path) as pdf:
         for page_num in range(len(pdf.pages)):
             page = pdf.pages[page_num]
             text = page.extract_text()
-            return f"\n{text}\n"
+            content+=f"\n{text}\n"
+    return content
 
 # print(get_text_from_pdf("uploads/try.pdf"))
